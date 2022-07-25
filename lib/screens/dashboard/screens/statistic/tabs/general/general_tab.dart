70493f997311b1230c723be90476bb429f3a5691
Ilia Rodionov <ilia.rodionov.mail@gmail.com>
2022-07-25 21:54:45 +0500

    [TIME-TRACKER-T-20] Поправил отображение статистики на телефоне

--- lib/screens/dashboard/screens/statistic/tabs/general/general_tab.dart
+++ lib/screens/dashboard/screens/statistic/tabs/general/general_tab.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:time_tracker_client/core/setup/injectable.dart';
+import 'package:time_tracker_client/core/widgets/responsive_utils.dart';
 import 'package:time_tracker_client/core/widgets/widget_with_top_loader.dart';
-import 'package:time_tracker_client/domain/usecase/statistic/general_statistic.dart';
 import 'package:time_tracker_client/screens/dashboard/screens/statistic/tabs/general/bloc/bloc.dart';
+import 'package:time_tracker_client/screens/dashboard/screens/statistic/tabs/general/widgets/general_statistic_list.dart';
+import 'package:time_tracker_client/screens/dashboard/screens/statistic/tabs/general/widgets/general_statistic_table.dart';
 
 class GeneralTab extends StatelessWidget {
   const GeneralTab({super.key});
@@ -37,9 +39,16 @@ class _GeneralTabBodyState extends State<_GeneralTabBody> {
       builder: (context, state) {
         final failure = state is FailedState ? state.failure : null;
         final isLoading = state is LoadingState;
-        final child = state is WithStatisticState
-            ? _GeneralStatisticTable(statistic: state.statistics)
-            : const SizedBox.shrink();
+        final Widget child;
+
+        if (state is WithStatisticState) {
+          child = ResponsiveWidget(
+            mobile: GeneralStatisticList(state.statistics),
+            desktop: GeneralStatisticTable(state.statistics),
+          );
+        } else {
+          child = const SizedBox.shrink();
+        }
 
         return WidgetWithTopLoader(
           isLoading: isLoading,
@@ -49,69 +58,4 @@ class _GeneralTabBodyState extends State<_GeneralTabBody> {
       },
     );
   }
-}
-
-class _GeneralStatisticTable extends StatelessWidget {
-  final GeneralStatistic statistic;
-
-  const _GeneralStatisticTable({required this.statistic});
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          DataTable(
-            headingRowHeight: 28,
-            headingRowColor: MaterialStateProperty.all(
-              const Color.fromRGBO(250, 250, 250, 1),
-            ),
-            border: TableBorder.symmetric(
-              outside:
-                  const BorderSide(color: Color.fromRGBO(220, 220, 220, 1)),
-            ),
-            columns: const <DataColumn>[
-              DataColumn(label: Text('Наименование')),
-              DataColumn(label: Text('В ч')),
-              DataColumn(label: Text('В %')),
-            ],
-            rows: _rows,
-          ),
-        ],
-      ),
-    );
-  }
-
-  List<DataRow> get _rows {
-    final rows = <DataRow>[];
-
-    for (var progress in statistic.projectProgress) {
-      rows.add(_GeneralStatisticRow(
-        progress.project.name,
-        progress.duration.toString(),
-        progress.formattedPercent,
-      ));
-    }
-
-    rows.add(_GeneralStatisticRow(
-      'Итого:',
-      statistic.total.toString(),
-      statistic.formattedPercent,
-    ));
-
-    return rows;
-  }
-}
-
-class _GeneralStatisticRow extends DataRow {
-  _GeneralStatisticRow(
-    String projectName,
-    String total,
-    String percent,
-  ) : super(color: MaterialStateProperty.all(Colors.white), cells: <DataCell>[
-          DataCell(Text(projectName)),
-          DataCell(Text(total.toString())),
-          DataCell(Text(percent)),
-        ]);
 }
---
+++ lib/screens/dashboard/screens/statistic/tabs/general/widgets/general_statistic_table.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_client/domain/usecase/statistic/general_statistic.dart';
+
+class GeneralStatisticTable extends StatelessWidget {
+  final GeneralStatistic statistic;
+
+  const GeneralStatisticTable(
+    this.statistic, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          DataTable(
+            headingRowHeight: 28,
+            headingRowColor: MaterialStateProperty.all(
+              const Color.fromRGBO(250, 250, 250, 1),
+            ),
+            border: TableBorder.symmetric(
+              outside:
+                  const BorderSide(color: Color.fromRGBO(220, 220, 220, 1)),
+            ),
+            columns: const <DataColumn>[
+              DataColumn(label: Text('Наименование')),
+              DataColumn(label: Text('В ч')),
+              DataColumn(label: Text('В %')),
+            ],
+            rows: _rows,
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<DataRow> get _rows {
+    final rows = <DataRow>[];
+
+    for (var progress in statistic.projectProgress) {
+      rows.add(_GeneralStatisticRow(
+        progress.project.name,
+        progress.duration.toString(),
+        progress.formattedPercent,
+      ));
+    }
+
+    rows.add(_GeneralStatisticRow(
+      'Итого:',
+      statistic.total.toString(),
+      statistic.formattedPercent,
+    ));
+
+    return rows;
+  }
+}
+
+class _GeneralStatisticRow extends DataRow {
+  _GeneralStatisticRow(
+    String projectName,
+    String total,
+    String percent,
+  ) : super(color: MaterialStateProperty.all(Colors.white), cells: <DataCell>[
+          DataCell(Text(projectName)),
+          DataCell(Text(total.toString())),
+          DataCell(Text(percent)),
+        ]);
+}
